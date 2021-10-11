c2645bdf3a5b194cddd7fb04ba41c5017f98dd77
mustafa <mustafaymustafa@icloud.com>
2021-10-11 22:23:03 +0100

    parse config file

---
+++ generator.py
@@ -0,0 +1,24 @@
+from entity import Entity
+
+import yaml
+
+def load_assets():
+    PATH = 'assets'
+
+    try:
+        assets = read_config()
+    except:
+        print("An exception occured reading the configuration file.")
+    finally:
+        print("Configuration successfully read.")
+
+
+def read_config():
+    config = yaml.load(open("config.yaml"), Loader=yaml.FullLoader)
+    return [Entity(**data) for data in config['ASSETS']]
+
+def main():
+    load_assets()
+
+if __name__ == "__main__":
+    main()
